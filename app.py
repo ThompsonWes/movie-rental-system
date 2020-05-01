2e93fcf2186eae8c2b88b84a81246aa25df2f2fe
John Funk <funkjo@my.easternct.edu>
2020-04-30 21:53:23 -0400

    add comments

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# app.py
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+app = Flask(__name__)  # create flask app
+app.config.from_object(Config)  # configures SQLALCHEMY_DATABASE_URI to sqlite:///mrs.db
+db = SQLAlchemy(app)  # create sqlalchemy database object from our flask app
--- models.py
+++ models.py
@@ -10,10 +10,10 @@ class User(db.Model):
     Users Entity
     """
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    dob = db.Column(db.Date)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,16 +25,19 @@ class CreditCard(db.Model):
     """
     cc_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    cc_number = db.Column(db.String(16), index=True)
-    cc_company = db.Column(db.String(64), index=True)
-    cc_expiration = db.Column(db.Date, index=True)
-    cc_cvc = db.Column(db.Integer)
+    cc_number = db.Column(db.String(16), index=True, nullable=False)
+    cc_company = db.Column(db.String(64), index=True, nullable=False)
+    cc_expiration = db.Column(db.Date, index=True, nullable=False)
+    cc_cvc = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<CC {}>'.format(self.body)
 
 
 def create_db():
+    """
+    Creates the database instance
+    """
     db.create_all()
 
 
